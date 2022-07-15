b84630a45ddfa35c419276118adc9215981cc52a
Ifeoluwa <65517123+hefeholuwah@users.noreply.github.com>
2022-07-15 07:42:45 +0000

    Iterated through all element and added to sum for task 1 iterated through all element and counted even numbers

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0 
+    for (let i = 0; i < arrayOfNumbers.length; i++)  
+           sum += arrayOfNumbers[i];
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let even_count = 0
+    for (let i = 0; i < arrayOfNumbers.length; i++)
+      if (arrayOfNumbers[i] % 2 == 0)
+        even_count++;
+    return even_count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,13 +54,12 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+    return arrayOfNumbers.map(item => Math.trunc((item * 1.8) + 32))
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
 // ========================
-
